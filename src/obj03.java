17da3381c36de4231d86a2ae07f0c36597b4dc9f
LeoAnders <leonardoanders15@gmail.com>
2023-12-02 15:20:42 -0300

    Initial commit

---
+++ src/obj03.java
@@ -0,0 +1,40 @@
+
+//Corrigindo problemas de repeticao de codigo e passando metodo de soma para entidade trinagulo
+import entities.Triangle;
+import java.util.Locale;
+import java.util.Scanner;
+public class obj03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x,y;
+        //Para que podemos usar o OBJ devemos in istanciar/criar usando o new
+        x = new Triangle();
+        y = new Triangle();
+
+        // Como faco para ler um numero inteiro e guardar no atributo A do OBJ X?
+        System.out.println("Enter the measures of triangle X: ");
+        //Colocaremos x. e ele ate mesmo auto ccompleta para escolher qual atributo queremos acessar
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        }
+        else {
+            System.out.println("Larger area: Y");
+        }
+        sc.close();
+    }
+}
---
+++ src/obj04ex.java
@@ -0,0 +1,44 @@
+//Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+//quantidade no estoque)
+
+import entities.Product;
+
+import java.util.Scanner;
+
+//Primeiro exportamos nossa classe Product
+public class obj04ex {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println("product data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("updated data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("updated data: " + product);
+
+        sc.close();
+    }
+}
